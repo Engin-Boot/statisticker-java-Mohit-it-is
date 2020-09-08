5ef7b0de47683c8746e76568b5db1b2bee01ca55
Mohit-it-is <mohitkr.mnnit@gmail.com>
2020-09-08 15:06:05 +0530

    Discussed with sourav

--- src/main/java/statisticker/Statistics.java
+++ src/main/java/statisticker/Statistics.java
@@ -1,25 +1,36 @@
 package statisticker;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Statistics 
 {
+	public static List<Float> RemoveNaNifPresent(List<Float> numbers){
+		
+		List<Float> new_list_of_numbers = new ArrayList<Float>(numbers);
+		new_list_of_numbers.removeAll(Collections.singleton(Float.NaN));
+		
+		return new_list_of_numbers;
+		
+	}
 	
     public static Stats getStatistics(List<Float> numbers) {
     	Stats stats = new Stats();
     	
-    	if(numbers.isEmpty())
+    	List<Float> list_without_NaN = RemoveNaNifPresent(numbers);
+    	
+    	if(list_without_NaN.isEmpty())
     		return stats;
     	
-    	stats.min = Collections.min(numbers);
-    	stats.max = Collections.max(numbers);
+    	stats.min = Collections.min(list_without_NaN);
+    	stats.max = Collections.max(list_without_NaN);
     	
     	Float sum = 0.0f;
-    	for(Float num: numbers) {
+    	for(Float num: list_without_NaN) {
     		sum = sum + num;
     	}
-    	stats.average = sum/numbers.size();
+    	stats.average = sum/list_without_NaN.size();
     	
 		return stats;
        
--- src/test/java/statisticker/StatisticsTest.java
+++ src/test/java/statisticker/StatisticsTest.java
@@ -20,9 +20,9 @@ public class StatisticsTest
         Stats s = Statistics.getStatistics(numberList);
 
         float epsilon = 0.001f;
-        assertTrue(Math.abs(s.average-4.525f)<epsilon);
-        assertTrue(Math.abs(s.min - 1.5f)<epsilon);
-        assertTrue(Math.abs(s.max - 8.9f)<epsilon);
+        assertTrue(Math.abs(s.average-4.525f) < epsilon);
+        assertTrue(Math.abs(s.min - 1.5f) < epsilon);
+        assertTrue(Math.abs(s.max - 8.9f) < epsilon);
     }
     
     @Test
@@ -37,6 +37,35 @@ public class StatisticsTest
         assertTrue(s.average.isNaN() == true);
         
     }
+    
+    @Test
+    public void reportwhenlistcontainsNaNasinput()
+    {
+        List<Float>	listContainingNaN = Arrays.asList(1.5f, 8.9f, 3.2f, 4.5f, Float.NaN);
+
+        Stats s = Statistics.getStatistics(listContainingNaN);
+        
+        float epsilon = 0.001f;
+        assertTrue(Math.abs(s.average-4.525f) < epsilon);
+        assertTrue(Math.abs(s.min - 1.5f) < epsilon);
+        assertTrue(Math.abs(s.max - 8.9f) < epsilon);
+        
+    }
+    
+    @Test
+    public void reportwhenlisthasallelementsasNaN()
+    {
+        List<Float>	listContainingonlyNaN = Arrays.asList(Float.NaN,Float.NaN,Float.NaN);
+
+        Stats s = Statistics.getStatistics(listContainingonlyNaN);
+        
+        assertTrue(s.min.isNaN() == true);
+        assertTrue(s.max.isNaN() == true);
+        assertTrue(s.average.isNaN() == true);
+        
+    }
+    
+    
     
     
 }
